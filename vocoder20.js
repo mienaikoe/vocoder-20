3f552fb26558edc2bfc5d8a4daef555391096d04
Jesse Lee <mienaikoe@gmail.com>
2015-02-27 01:48:59 -0500

    a bit o tunning

--- vocoder20.js
+++ vocoder20.js
@@ -4,17 +4,17 @@ anp=AudioNode.prototype;anp.to=anp.connect;n=navigator;n.getUserMedia=
 (n.getUserMedia||n.webkitGetUserMedia||n.mozGetUserMedia||n.msGetUserMedia);
 n.getUserMedia({audio:true},function(str){mic=c.createMediaStreamSource(str);
 for(i=65;i<91;i++){oz=c.createOscillator();oz.type='sawtooth';oz.start();
-oz.frequency.value=8*M.pow(2,i/12);z=c._G();z.gain.value=0;oz.to(z);ox[i]=z;
+oz.frequency.value=4*M.pow(2,i/12);z=c._G();z.gain.value=0;oz.to(z);ox[i]=z;
 hb=d.ce("div");hv=hb.style;d.body.appendChild(hb);hx[i]=hv;hv.opacity=0;
-hb.innerHTML=String.fromCharCode(i);}d.body.style.height="100%";d.m=mic;
+hb.innerHTML=String.fromCharCode(i);}d.body.style.height="100%";
 cv=new Float32Array(fm);for(i=-fh;i<fh;i++){cv[i+fh]=Math.max(i<0?-i:i,0.2)/fh;}
 for(i=0;i<40;i++){fr=20*M.pow(2,i/4);mb=c._F();mb.type="bandpass";mic.to(mb);
 mb.frequency.value=fr;mr=c._W();mr.curve=cv;mf=c._F();mf.type='LOWPASS';
-ob=c._F();ob.type="bandpass";mf.frequency.value=10;ob.frequency.value=fr;
+ob=c._F();ob.type="bandpass";mf.frequency.value=fr/50;ob.frequency.value=fr;
 og=c._G();og.gain.value=0;mb.to(mr);mr.to(mf);mf.to(og.gain);mb.Q.value=8;
 for(ix in ox){ox[ix].to(ob);}ob.to(og);og.to(c.destination);}
-w=window;w.a=w.addEventListener;function et(v){return function(ev){
-_=ev.keyCode;if(_>64 && _<91){ox[_].gain.value=v;hx[_].opacity=v;}};}
+w=window;w.a=w.addEventListener;function et(v){return function(ev){_=ev.keyCode;
+if(_>64 && _<91){ox[_].gain.value=v;hx[_].opacity=v;}};};
 w.a("keydown",et(0.5));w.a("keyup",et(0));},function(){});
 ho="<p style='margin:3%;float:right;clear:both;'>";hc="</p>";
 d.body.innerHTML=ho+"Keys = Play Note"+hc+ho+"Microphone = Modulate"+hc;
